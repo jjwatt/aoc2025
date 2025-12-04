0eb33feaa9ca43d9ae5e488f250861f550cb5b91
Jesse Wattenbarger <jwatt@broken.watch>
2025-12-04 14:00:05 -0500

    Simplified and improved day 2 solution

--- puzzle2.py
+++ puzzle2.py
@@ -1,4 +1,5 @@
 """Advent of Code 2025: Puzzle 2, part 1 and part 2."""
+import itertools
 import re
 from collections.abc import Iterable
 
@@ -17,45 +18,40 @@ def range_from_str(range_str: str):
     Args:
         range_str: Like, "1-10"
     """
-    try:
-        start_str, end_str = range_str.split('-')
-        start, end = int(start_str), int(end_str)
-    except ValueError as ve:
-        print(f"Bad range string: {ve}")
+    start, end = map(int, range_str.split('-'))
     return range(start, end)
 
 
-def gen_ranges(ranges_str: Iterable):
-    """Generate ranges of ids."""
-    for range_str in ranges_str:
-        yield range_from_str(range_str)
+def is_invalid_id(num: int) -> bool:
+    """Check the puzzle condition.
+
+    Does 2nd half == 1st half?
+    """
+    s = str(num)
+    mid = len(s) // 2
+    return s[:mid] == s[mid:]
 
 
 def gen_invalid_ids(ranges: Iterable):
     """Generate invalid ids for 1st part of puzzle."""
-    for range in ranges:
-        for id in range:
-            str_id = str(id)
-            str_id_len = len(str_id)
-            half_len = str_id_len // 2
-            if str_id[half_len:] == str_id[:half_len]:
-                yield id
+    ranges = map(range_from_str, ranges)
+    all_ids = itertools.chain.from_iterable(ranges)
+    return (i for i in all_ids if is_invalid_id(i))
 
 
 def gen_invalid_ids2(ranges: Iterable):
     """Generate invalid ids for 2nd part of puzzle."""
-    for range in ranges:
-        for id in range:
-            if re.match(r'(\d+)\1+$', str(id)):
-                yield id
+    ranges = map(range_from_str, ranges)
+    all_ids = itertools.chain.from_iterable(ranges)
+    return (i for i in all_ids if re.match(r'(\d+)\1+$', str(i)))
 
 
 def main():
     """Run the main body of the script."""
-    invalid_ids = gen_invalid_ids2(
-        gen_ranges(gen_str_ranges("input2.txt"))
-    )
-    print(f"sum(invalid_ids): {sum(invalid_ids)}")
+    invalid_ids1 = gen_invalid_ids(gen_str_ranges("input2.txt"))
+    invalid_ids2 = gen_invalid_ids2(gen_str_ranges("input2.txt"))
+    print(f"sum(invalid_ids1): {sum(invalid_ids1)}")
+    print(f"sum(invalid_ids2): {sum(invalid_ids2)}")
 
 
 if __name__ == "__main__":
