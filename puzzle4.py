025153967d9f4574f59a494cff4f35e80e2d8de1
Jesse Wattenbarger <jwatt@broken.watch>
2025-12-07 13:21:40 -0500

    Day 4 with part 1 and part 2.

--- puzzle4.py
+++ puzzle4.py
@@ -1,4 +1,5 @@
 """Advent of Code puzzle4 part 1."""
+import copy
 
 
 def gen_input(filepath):
@@ -38,7 +39,7 @@ def check_neighbors(grid, row, col):
     return neighbors < 4
 
 
-def check_grid(grid):
+def check_grid_accessible(grid):
     """Check for the number of accessible slots in the grid."""
     rows = len(grid)
     cols = len(grid[0])
@@ -51,12 +52,43 @@ def check_grid(grid):
     return accessible
 
 
+def get_removable(grid):
+    """Check for the accessible slots in the grid."""
+    rows = len(grid)
+    cols = len(grid[0])
+    removable = []
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == '@':
+                if check_neighbors(grid, row, col):
+                    removable.append((row, col))
+    return removable
+
+
+def check_total_removed(grid):
+    """Find the total number we can remove by repeating removals."""
+    mygrid = copy.copy(grid)
+    total_removed = 0
+    while True:
+        removable = get_removable(mygrid)
+        if not removable:
+            break
+        total_removed += len(removable)
+        for row, col in removable:
+            mygrid[row][col] = '.'
+    return total_removed
+
+
 def main():
     """Run the main body of the script."""
     lines = list(gen_input("p4-full-input.txt"))
     grid = build_grid(lines)
-    accessible = check_grid(grid)
-    print(f"{accessible=}")
+    print("Part1:")
+    accessible = check_grid_accessible(grid)
+    print(f"\t{accessible=}")
+    print("Part2:")
+    total_removed = check_total_removed(grid)
+    print(f"\t{total_removed=}")
 
 
 if __name__ == "__main__":
