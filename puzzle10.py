faad93b988a56376af14993854573276f8165f4c
Jesse Wattenbarger <jwatt@broken.watch>
2025-12-16 18:46:28 -0500

    Working on solution for Day 10, part 1

--- puzzle10.py
+++ puzzle10.py
@@ -1,40 +1,56 @@
 """Advent of Code 2025 Day 10, part 1."""
 import re
+from collections import deque
 
-def get_input(filepath):
+
+def gen_input(filepath):
     """Generate the input."""
-    lines = []
     with open(filepath, 'r') as f:
-        lines = [line.rstrip('\n') for line in f]
-    return lines
+        for line in f:
+            yield line.rstrip('\n')
 
 
-def get_targets(lines):
-    """Get the target strings."""
-    targets = []
+def gen_parsed_strings(lines):
+    """Generate extracted strings.
+
+    Extract target pattern. e.g., ".##." and buton pattern. e.g.,
+    ["0,3", "1,2"].
+    """
     for line in lines:
-        target = re.search(r'\[(.*?)\]', line).group(1)
-        targets.append(target)
-    return targets
+        target_str = re.search(r'\[(.*?)\]', line).group(1)
+        button_strs = re.findall(r'\((.*?)\)', line)
+        yield target_str, button_strs
 
 
-def get_buttons(lines):
-    all_buttons = []
-    for line in lines:
-        buttons = re.findall(r'\((.*?)\)', line)
-        all_buttons.append(buttons)
-    return all_buttons
+def gen_bitmasks(parsed_data):
+    """Generate integer bitmasks from parsed string data."""
+    for target_str, button_strs in parsed_data:
+        target_mask = 0
+        for idx, char in enumerate(target_str):
+            if char == '#':
+                target_mask |= (1 << idx)
+        button_masks = []
+        for button_str in button_strs:
+            mask = 0
+            if button_str:
+                indices = [int(x) for x in button_str.split(',')]
+            for idx in indices:
+                mask |= (1 << idx)
+            button_masks.append(mask)
+        yield target_mask, button_masks
+
+
+def gen_solutions(machines):
+    """Generate the shortest path from 0 (all off) to target using BFS."""
 
 
 def main():
     filename = "p10-sample-input.txt"
-    lines = get_input(filename)
-    print(f"{lines=}")
-    targets = get_targets(lines)
-    print(f"{targets=}")
-    buttons = get_buttons(lines)
-    print(f"{buttons=}")
-    
+    parsed_strings = list(gen_parsed_strings(gen_input(filename)))
+    print(f"{parsed_strings=}")
+    machines = gen_bitmasks(parsed_strings)
+    print(f"{list(machines)=}")
+
 
 if __name__ == "__main__":
     main()
